9a9c8a6e5008708b7b010d7c3469ee5c06a1bc22
steve <stev8855@gmail.com>
2024-09-06 09:47:45 -0500

    feat: Add ProfileView widget to screens.dart

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -4,3 +4,5 @@ export 'package:chambeape/presentation/screens/0_login/login_view.dart';
 export 'package:chambeape/presentation/screens/1_home/home_view.dart';
 export 'package:chambeape/presentation/screens/2_workers/workers_view.dart';
 export 'package:chambeape/presentation/screens/3_posts/post_view.dart';
+
+export 'package:chambeape/presentation/screens/5_profile/profile_view.dart';
