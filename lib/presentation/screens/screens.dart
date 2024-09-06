89fbf9f857186da42b8b58b2165373eeee6c05aa
steve <stev8855@gmail.com>
2024-09-06 09:20:51 -0500

    feat: Export PostView widget for displaying posts and managing post creation/editing

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,4 @@
+export 'package:chambeape/presentation/screens/0_login/login_view.dart';
 export 'package:chambeape/presentation/screens/1_home/home_view.dart';
 export 'package:chambeape/presentation/screens/2_workers/workers_view.dart';
-export 'package:chambeape/presentation/screens/0_login/login_view.dart';
+export 'package:chambeape/presentation/screens/3_posts/post_view.dart';
