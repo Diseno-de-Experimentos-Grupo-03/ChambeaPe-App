7afc876d48bdc75b8012ff13481acbcd14aa96f6
steve <stev8855@gmail.com>
2024-09-06 09:45:32 -0500

    feat: Add options view to screens.dart

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,5 @@
+export 'package:chambeape/presentation/screens/options/options_view.dart';
+
 export 'package:chambeape/presentation/screens/0_login/login_view.dart';
 export 'package:chambeape/presentation/screens/1_home/home_view.dart';
 export 'package:chambeape/presentation/screens/2_workers/workers_view.dart';
