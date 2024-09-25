e71865f984e6e9bfd221992af9c16f7e6bb87ab9
Diego Esquivel <u202116749@upc.edu.pe>
2024-09-25 18:28:57 -0500

    feat: add chat test

--- lib/services/chat/chat_list_service.dart
+++ lib/services/chat/chat_list_service.dart
@@ -5,11 +5,14 @@ import 'package:http/http.dart' as http;
 class ChatListService {
   final String baseUrl =
       'https://chambeape-chat.azurewebsites.net/api/chatroom';
+  final http.Client? client;
+
+  ChatListService({this.client});
 
   Future<List<String>> getExistingChatUsersId(String userId) async {
     String url = '$baseUrl/$userId/users';
     final uri = Uri.parse(url);
-    final response = await http.get(uri);
+    final response = await (client ?? http.Client()).get(uri);
 
     if (response.statusCode >= 200 && response.statusCode < 300) {
       List<String> ids =
